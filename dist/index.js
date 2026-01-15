720cec721da0ec91a4ad253048ca8d50a626e95d
Krzysztof Surażyński Prywata <krzysztofsurazynskiprywata@192.168.1.3>
2026-01-15 22:44:47 +0100

    deleted dist

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// This file serves as the entry point for the TypeScript application. It may contain the main logic or initialization code for the project.
-console.log("Welcome to the Sieci Społecznościowe project!");
